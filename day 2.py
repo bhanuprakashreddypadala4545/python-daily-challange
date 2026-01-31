af0e06b931ecad47e9e911d09bec38c1566a5fed
bhanuprakashreddypadala4545 <bhanuprakashreddypadala4545@gmail.com>
2026-01-31 19:59:47 +0530

    Update day 2.py

--- day 2.py
+++ day 2.py
@@ -1,34 +1,19 @@
-student_ID = input("Enter student ID : ")
-email_ID = input("Enter student email : ")
-password = input("Enter password : ")
-Referral_code = input("Enter Referral code : ")
-valid = True
-if  len(student_ID) ==7 and student_ID[0:3] == "CSE"  and student_ID[3] == "-" and student_ID[-3:].isdigit() :
-      pass
-else:
-      valid = False
+studentid=input("Enter Studentid :")
+emailid=input("Enter Emailid :")
+password=input("Enter Password :")
+referral=input("Enter Referral :")
 
-if "@" in email_ID and "." in email_ID :
-     if email_ID[0] !="@" and email_ID[-1] !="@" and email_ID[-4: ] == ".edu" :
-         pass
-     else :
-         valid = False
-else :
-     valid = False
-
-if len(password)>=8 :
-    if 'A' <= password[0] <= 'Z' and any(ch.isdigit() for ch in password) :
-         pass
-    else :
-        valid = False
-else:
-    valid = False
-if len(Referral_code) == 6 and Referral_code[0:3] == "REF" and Referral_code[3 : 5].isdigit()  and Referral_code[-1] == "@" :
-    pass
-else:
-    valid = False
-
-if valid:
-    print("APPROVED")
+if studentid[0:3]=="CSE" and studentid[3]=="-" and studentid[4:7].isdigit():
+    if emailid.count("@")>=1 and emailid.count(".")>=1 and emailid[-4:]==".edu" and emailid[len(emailid)-1]!="@" and emailid[0]!="@":
+        if len(password)>=8 and password[0].isupper() and (password.count ('0') or password.count ('1') or password.count ('2') or password.count ('3') or password.count ('4') or
+                                                           password.count('5') or password.count('6') or password.count('7') or password.count('8') or password.count('9') ):
+            if referral[0:3]=="REF" and referral[3:5].isdigit() and referral[5]=="@":
+                print("APPROVED")
+            else:
+                print("REJECTED")
+        else:
+            print("REJECTED")
+    else:
+        print("REJECTED")
 else:
     print("REJECTED")
