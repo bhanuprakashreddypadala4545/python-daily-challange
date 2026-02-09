9319439991b06b5887c4e9ce1e08a3bc480b1085
bhanuprakashreddypadala4545 <bhanuprakashreddypadala4545@gmail.com>
2026-02-09 19:12:11 +0530

    Add files via upload

---
+++ day 3.py
@@ -0,0 +1,28 @@
+
+n=int(input("enter no.of students:"))
+m=[0]*n
+for i in range(n):
+    m[i]=int(input("enter marks:"))
+totalvalidstudents=0
+totalfailstudents=0
+for i in range(n):
+    if m[i]<0 or m[i]>100 :
+        print(m[i],"->invalid")
+    else :
+         totalvalidstudents+=1
+         if m[i]>=90:
+           print(m[i],"->excellent")
+         elif m[i]>=75:
+           print(m[i],"->verygood")
+         elif m[i]>=60:
+             print(m[i],"->good")
+         elif m[i]>=40:
+             print(m[i],"->average")
+         else:
+             totalfailstudents+=1
+             print(m[i],"->fail")
+
+
+
+print("total valid students are :",totalvalidstudents)
+print("total fail students are:",totalfailstudents)
